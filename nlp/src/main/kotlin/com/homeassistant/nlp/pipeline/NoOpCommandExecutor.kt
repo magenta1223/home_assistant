5795dd84dfc2b41caa4875601630a1006922dd41
dongh <magenta1223@naver.com>
2026-04-09 14:16:21 +0900

    feat: Introduce Toolcall

--- nlp/src/main/kotlin/com/homeassistant/nlp/pipeline/NoOpCommandExecutor.kt
+++ nlp/src/main/kotlin/com/homeassistant/nlp/pipeline/NoOpCommandExecutor.kt
@@ -1,9 +1,9 @@
 package com.homeassistant.nlp.pipeline
 
 import com.homeassistant.core.tools.IToolExecutor
-import com.homeassistant.core.commands.ToolResult
 import com.homeassistant.core.commands.UserId
 import com.homeassistant.core.tools.ToolCallSpec
+import com.homeassistant.core.tools.ToolResult
 
 
 class NoOpToolExecutor : IToolExecutor {
